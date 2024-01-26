0ea569d7ee7d1fb9f95e20de343b1c4bb7a9b0ff
luaan <luandophu8114@gmail.com>
2024-01-26 15:42:20 +0700

    push class

---
+++ testGit/Demo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testGit
+{
+    internal class Demo
+    {
+        int x = 1;
+        int y = 2;
+        int z = 1 + 2;
+    }
+}
